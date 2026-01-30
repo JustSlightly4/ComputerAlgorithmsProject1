0aab221ba097dbdc10ca0e46ed93b0f29dbd4db7
Eric Ryan Montgomery <ermontgomery1@crimson.ua.edu>
2026-01-30 14:55:25 -0600

    Implemented Prims Algorithm

--- Driver.cpp
+++ Driver.cpp
@@ -1,9 +1,55 @@
-//#include "PrimsAlgorithm.h"
+#include "PrimsAlgorithm.h"
 #include "Heap.h"
 #include "FibonacciHeap.h"
 #include "PairingHeap.h"
 #include <vector>
+#include <ostream>
 using namespace std;
+
+//Easy printing of a vector for myself
+template <typename T>
+ostream& operator<<(ostream &os, const vector<T> &vect) {
+
+	//Safety Check
+	if (vect.size() <= 0) return os;
+
+	//Push vector contents into the ostream
+	os << vect[0]; 
+	for (unsigned int i = 1; i < vect.size(); ++i) {
+		os << ", " << vect[i];
+	}
+
+	//Return the stream(?)
+	return os;
+}
+
+//Easy printing of a 2D vector for myself
+template <typename T>
+ostream& operator<<(ostream &os, const vector<vector<T>> &vect) {
+
+	//Safety Check
+	if (vect.size() <= 0) return os;
+
+    for (unsigned int i = 0; i < vect.size(); ++i) {
+        for (unsigned int j = 0; j < vect[i].size(); ++j) {
+            os << vect[i][j] << ", ";
+        }
+        os << "\n";
+    }
+
+	//Return the stream(?)
+	return os;
+}
+
+template <typename T>
+void interpretMinimumSpanningTree(vector<vector<T>> const &graph) {
+    for (int i = 0; i < graph.size(); ++i) {
+        for (int j = 0; j < graph[i].size(); ++j) {
+            if (graph[i][j] != 0) cout << "{" << i << " -> " << j << " = " << graph[i][j] << "} ";
+        }
+        cout << "\n";
+    }
+}
 
 int main() {
 
@@ -13,6 +59,6 @@ int main() {
         {1, 0, 2},
         {3, 2, 0},
     };
-    //cout << PrimsAlgorithm<Heap>(myList) << "\n";
+    interpretMinimumSpanningTree(PrimsAlgorithm<Heap>(myList));
     return 0;
 }
--- PrimsAlgorithm.h
+++ PrimsAlgorithm.h
@@ -2,49 +2,34 @@
 #define PRIMSALGORITHMH
 #include <vector>
 #include <limits>
+#include <utility>
 using namespace std;
 
 template <template <typename, typename> class HeapType>
-int PrimsAlgorithm(const vector<vector<int>>& matrix) {
-    if (matrix.size() < 1) return 0;
+vector<vector<int>> PrimsAlgorithm(const vector<vector<int>>& graph) {
+    if (graph.empty()) return {};
 
-    //Set an infinity value to the maximum integer
+    HeapType<int, int> minimumEdges;
+    vector<vector<int>> minimumSpanningTree(graph.size(), vector<int>(graph.size(), 0));
     const int INF = numeric_limits<int>::max();
 
-    //Save the size of the matrix
-    int size = matrix.size();
-
-    vector<bool> inMST(size, false);
-    HeapType<int, int> heap;
-
-
-    // Insert all vertices into heap
-    heap.insert(0, 0); //Insert the starting node that cost 0 to travel to itself
-    for (int i = 1; i < size; ++i) {
-        heap.insert(i, INF); //Set all other nodes to infinity for now
+    minimumEdges.insert(0, 0);
+    for (int i = 1; i < graph.size(); ++i) {
+        minimumEdges.insert(i, INF);
     }
 
-    int mstWeight = 0;
+    while(!minimumEdges.empty()) {
+        int minimumValue = minimumEdges.GetMinValue();
+        int minimumKey = minimumEdges.GetMinKey();
+        minimumEdges.extractMin();
+        if (minimumValue == INF) continue;
 
-    while (!heap.empty()) {
-        int minimumNodeKey = heap.GetMinKey();
-        int minimumNodeValue = heap.GetMinValue();
-        heap.extractMin();
-        inMST[minimumNodeKey] = true;
-
-        if (minimumNodeValue != INF) mstWeight += minimumNodeValue;
-
-        // Relax edges from the minimum node
-        for (int i = 0; i < size; ++i) {
-            int weight = matrix[minimumNodeKey][i];
-
-            if (weight != 0 && !inMST[i] && weight < key[i]) {
-                key[i] = weight;
-                heap.decreaseKey(i, weight);
-            }
+        for (int i = 0; i < graph.size(); ++i) {
+            if (graph[minimumKey][i] != 0) minimumEdges.decreaseKey(i, graph[minimumKey][i]);
         }
-    }
 
-    return mstWeight;
+        if (!minimumEdges.empty()) minimumSpanningTree[minimumKey][minimumEdges.GetMinKey()] = minimumEdges.GetMinValue();
+    }
+    return minimumSpanningTree;
 }
 #endif
